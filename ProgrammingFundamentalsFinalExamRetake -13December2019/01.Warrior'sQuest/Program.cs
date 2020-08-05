6848b5b5bcc9395cf743ec1754b9256b854696b4
Rumen Sinigerski <69121051+RumenSinigerski@users.noreply.github.com>
2020-08-05 22:41:23 +0300

    Add files via upload

---
+++ ProgrammingFundamentalsFinalExamRetake -13December2019/01.Warrior'sQuest/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _01.Warrior_sQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder skill = new StringBuilder(Console.ReadLine());
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "For Azeroth")
+            {
+                var command = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (command.Contains("GladiatorStance"))
+                {
+                    skill = new StringBuilder(skill.ToString().ToUpper());
+                    Console.WriteLine(skill);
+                }
+                else if (command.Contains("DefensiveStance"))
+                {
+                    skill = new StringBuilder(skill.ToString().ToLower());
+                    Console.WriteLine(skill);
+                }
+                else if (command.Contains("Dispel"))
+                {
+                    int index = int.Parse(command[1]);
+                    char replacement = char.Parse(command[2]);
+                    if (index >= 0 && index < skill.Length)
+                    {
+                        skill = skill.Replace(skill[index], replacement, index, 1);
+                        Console.WriteLine("Success!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dispel too weak.");
+                    }
+                }
+                else if (command.Contains("Target") && command.Contains("Change"))
+                {
+
+                    string firstSubstring = command[2];
+                    string secondSubstring = command[3];
+
+                    if (skill.ToString().Contains(firstSubstring))
+                    {
+                        skill = skill.Replace(firstSubstring, secondSubstring);
+                        Console.WriteLine(skill);
+                    }
+
+
+                }
+                else if (command.Contains("Target") && command.Contains("Remove"))
+                {
+                    string substring = command[2];
+                    if (skill.ToString().Contains(substring))
+                    {
+                        int index = skill.ToString().IndexOf(substring);
+                        skill = skill.Remove(index, substring.Length);
+                        Console.WriteLine(skill);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Command doesn't exist!");
+                }
+
+                
+            }
+        }
+    }
+}
---
+++ ProgrammingFundamentalsFinalExamRetake -13December2019/03.HeroRecruitment/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.HeroRecruitment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> heroSpells = new Dictionary<string, List<string>>();
+
+            var command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (!command.Contains("End"))
+            {
+                string hero = command[1];
+                if (command.Contains("Enroll"))
+                {
+                    if (!heroSpells.ContainsKey(hero))
+                    {
+                        heroSpells.Add(hero, new List<string>());
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hero} is already enrolled.");
+                    }
+                }
+                else if (command.Contains("Learn"))
+                {
+                    string spell = command[2];
+
+                    if (!heroSpells.ContainsKey(hero))
+                    {
+                        Console.WriteLine($"{hero} doesn't exist.");
+                    }
+                    else
+                    {
+                        if (heroSpells[hero].Contains(spell))
+                        {
+                            Console.WriteLine($"{hero} has already learnt {spell}.");
+                        }
+                        else
+                        {
+                            heroSpells[hero].Add(spell);
+                        }
+                    }
+                }
+                else if (command.Contains("Unlearn"))
+                {
+                    string spell = command[2];
+
+                    if (!heroSpells.ContainsKey(hero))
+                    {
+                        Console.WriteLine($"{hero} doesn't exist.");
+                    }
+                    else
+                    {
+                        if (!heroSpells[hero].Contains(spell))
+                        {
+                            Console.WriteLine($"{hero} doesn't know {spell}.");
+                        }
+                        else
+                        {
+                            heroSpells[hero].Remove(spell);
+                        }
+                    }
+                }
+
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            heroSpells = heroSpells.OrderByDescending(v => v.Value.Count).ThenBy(k => k.Key).ToDictionary(k => k.Key, v => v.Value);
+
+            Console.WriteLine("Heroes:");
+            foreach (var item in heroSpells)
+            {
+                Console.WriteLine($"== {item.Key}: {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
